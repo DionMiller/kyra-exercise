07787f5d137cf3fdf3457766455e15ec3e2a9597
DionMiller <101334053+DionMiller@users.noreply.github.com>
2022-12-07 00:57:40 +0000

    update

--- backend/api.js
+++ backend/api.js
@@ -8,54 +8,186 @@
 
 const express = require("express")
 const app = express()
-const port = 4000
+const axios = require('axios');
 const cors = require("cors")
+const dotenv = require('dotenv');
+const { response } = require("express");
+dotenv.config()
 const sdk = require('api')('@fsq-developer/v1.0#5ht27ul9n9ebnn')
-'fsq3Wy3+RUvdvcRLm4DkXbqHwQjIUUjaZcaQNxBWYjuZGhE='
-const fetchPlacesPictures = () =>{
-    sdk.auth('fsq3Wy3+RUvdvcRLm4DkXbqHwQjIUUjaZcaQNxBWYjuZGhE=');
+
+// 'fsq3Wy3+RUvdvcRLm4DkXbqHwQjIUUjaZcaQNxBWYjuZGhE='
+const fetchPlacesPictures = async () =>{
+    sdk.auth(process.env.FSQ_API_KEY);
     sdk.placePhotos({limit: '1', classifications: 'outdoor', fsq_id: '5b684dab112c6c002cfd309d'})
       .then(({ data }) => console.log(data))
       .catch(err => console.error(err));
 }
+const locationOptions = {
+    method: 'GET',
+    url: 'https://api.foursquare.com/v3/places/search?query=coffee',
+    headers: {
+        accept: 'application/json',
+        Authorization: 'fsq3Wy3+RUvdvcRLm4DkXbqHwQjIUUjaZcaQNxBWYjuZGhE='
+    }
+    };
 
-
-async function fetchPlaces() {
-    try {
-        const searchParams = new URLSearchParams({
-          query: 'coffee',
-          open_now: 'true',
-          sort: 'DISTANCE'
-        });
-        const results = await fetch(
-          `https://api.foursquare.com/v3/places/search?${searchParams}`,
-          {
+const locations = async (locationReq) => {
+    return axios.request(locationReq).then(response => response.data.results.map((location)=>{
+        const options = {
             method: 'GET',
             headers: {
-              Accept: 'application/json',
-              Authorization: 'fsq3Wy3+RUvdvcRLm4DkXbqHwQjIUUjaZcaQNxBWYjuZGhE=',
+                accept: 'application/json',
+                Authorization: 'fsq3Wy3+RUvdvcRLm4DkXbqHwQjIUUjaZcaQNxBWYjuZGhE='
             }
-          }
-        );
-        const data = await results.json();
-        console.log(JSON.stringify(data.results))
-        // console.log(data.results)
-        return data.results;
-    } catch (err) {
-        console.error(err);
     }
+        options.url = `https://api.foursquare.com/v3/places/${location.fsq_id}/photos?limit=1`
+        
+        return (options)
+    }
+    
+    ))
+    }
+// const pictures = async (locationReq) =>{
+//     let data = locationReq.map((picture) => axios.request(picture).then(response=>response.data))
+//     console.log(data)
+//     }
+
+
+const pictures = async (locationReq) =>{
+    let finalData = []
+    locationReq.forEach(item=>axios.request(item).then(response=>{
+        finalData.push(response.data)
+        return(finalData)
+    })
+    )
+    console.log(finalData)
 }
+    
+
+
+        
+const fetchAxios = async () =>{
+    console.log("running")
+    const locationlist = await locations(locationOptions)
+   
+    const pictureList = await pictures(locationlist)
+    
+    // console.log(locationlist)
+    // return(await pictureList)
+}
+
+    
+
+// async function fetchPlaces() {
+//     try {
+//         const searchParams = new URLSearchParams({
+//           query: 'coffee',
+//           open_now: 'true',
+//           sort: 'DISTANCE'
+//         });
+//         const results = await fetch(
+//           `https://api.foursquare.com/v3/places/search?${searchParams}`,
+//           {
+//             method: 'GET',
+//             headers: {
+//               Accept: 'application/json',
+//               Authorization: 'fsq3Wy3+RUvdvcRLm4DkXbqHwQjIUUjaZcaQNxBWYjuZGhE=',
+//             }
+//           }
+//         );
+//         const data = await results.json();
+//         console.log(JSON.stringify(data.results))
+//         // console.log(data.results)
+//         return data.results;
+//     } catch (err) {
+//         console.error(err);
+//     }
+// }
 app.use(cors())
-app.listen(port, () => {
-	console.log(`Listening at http://localhost:${port}`)
+app.listen(process.env.PORT, () => {
+	console.log(`Listening at http://localhost:${process.env.PORT}`)
 })
 
 app.get("/",cors(),async(req,res)=>{
-	let places = fetchPlaces()
-    // places.then(res.send.bind(res))
-    fetchPlaces()
-        .then(data =>res.status(200).send(data))
-    
-    // res.send("hi")
-    // console.log(places)
+
+    fetchAxios()
+        .then(data =>res.status(200).send(JSON.stringify(data)))
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // return axios.request(locationOptions).then(response => console.log(response.data))
+// axios.all([locationRequest, pictureRequest]).then((data) => {
+//     data.forEach(item=> console.log(item.data))
+//  }).catch(error => {
+//     console.error(error)
+// })
+
+// const optionList = await locations()
+// console.log(optionList)
+
+//   const newlists =[axios.request(newlist[0]),axios.request(newlist[1])]
+// axios.all(newlists.map((endpoint) => axios.request(endpoint))).then(
+//     axios.spread(function (...res){
+//         console.log(res.data)}
+//   ))
+  
+
+    
+//   axios.request(testReq).then(response=> console.log(response.data))
+// const fetchAllLocations = async (list) => {axios.all(list.map(option => {axios.request(option)}))
+// .then((data) => {
+//         data.forEach(item=> console.log(item))
+//         })
+// .catch(error => {
+//         console.error(error)
+// })}
+
+    // await locations().then(fetchAllLocations(locations()))
+    // console.log( await locations())
+
+    // await (fetchAllLocations(locations()))
+
+    
+    
+
+
+
+// axios
+//   .request(locationOptions)
+//   .then(function (response) {
+  
+//     // console.log(response.data.results);
+//     // let arr1 = response.data.results.map((location)=>location.fsq_id)
+//     axios
+//     .all(response.data.results.map((fsq_id)=>axios.request({
+//         method: 'GET',
+//         url: `https://api.foursquare.com/v3/places/${fsq_id}/photos?limit=1&classifications=outdoor`,
+//         headers: {
+//           accept: 'application/json',
+//           Authorization: 'fsq3Wy3+RUvdvcRLm4DkXbqHwQjIUUjaZcaQNxBWYjuZGhE='
+//         }
+//       })))
+//     .then(
+//         (data)=>data.forEach(item=> console.log(item))
+//     )
+//     .catch(error => {
+//             console.error(error)
+//         })
+//     console.log(arr1)
+//   })
+//   .catch(function (error) {
+//     console.error(error);
+//   });
+  
